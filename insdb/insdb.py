ed6d74fa9cfd229cfabb1cbcf28758f51137ca60
nishalr <thearchit@hotmail.com>
2013-05-29 20:49:31 +0100

    Removed debugging statements from insdb.py

--- insdb/insdb.py
+++ insdb/insdb.py
@@ -10,18 +10,15 @@ def gethost():
 
     return 'localhost'
 
-def incr_counter(db_name, db_table, col_name):
-    pass
 
 def main():
-    debug = 0
     uid = os.getuid()
     db_user = os.getlogin()
     db_host = gethost()
     db_password = 'UEf4JBvAfY'
     db_name = 'insdb'
     db_table = db_name + '.uniqid'
-    def_values = (uid, db_user, 1)
+    def_values = (uid, db_user, 0)
 
     db_access = {
             'user': db_user,
@@ -46,31 +43,22 @@ def main():
             'SELECT counter FROM {} WHERE uid = {};'.format(db_table, uid)
             )
 
-    sql_update_counter = (
-            'UPDATE {} set counter = {} '
-            'WHERE uid = {};'
-            )
+    sql_update_counter = 'UPDATE {} set counter = {} WHERE uid = {};'
 
-    if debug == True:
-        print 'MySQL Connection string:\n', db_access, '\n'
-        print 'MySQL Create Statement:\n', sql_create_table, '\n'
-        print 'MySQL Insert Statement:\n', sql_insert_row, '\n'
-        print 'MySQL Select Statement:\n', sql_read_counter, '\n'
-        print 'MySQL Update Statement:\n', sql_update_counter, '\n'
-    else:
-        db_connect = mysql.connector.connect(**db_access)
-        db_cursor = db_connect.cursor()
-        db_cursor.execute(sql_create_table)
-        try:
-            db_cursor.execute(sql_insert_row)
-        except:
-            db_cursor.execute(sql_read_counter)
-            counter = db_cursor.fetchall()[0]
-            db_cursor.execute(sql_update_counter.format(db_table,
-                counter[0] + 1, uid))
+    db_connect = mysql.connector.connect(**db_access)
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(sql_create_table)
 
-        db_cursor.close()
-        db_connect.close()
+    try:
+        db_cursor.execute(sql_insert_row)
+    except:
+        db_cursor.execute(sql_read_counter)
+        counter = db_cursor.fetchall()[0]
+        db_cursor.execute(sql_update_counter.format(db_table,
+            counter[0] + 1, uid))
+
+    db_cursor.close()
+    db_connect.close()
 
 if __name__ == '__main__':
     main()
